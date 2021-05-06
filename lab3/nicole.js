8151079daa617b2055c4b47bcdb3055fc2c5d6f1
mattzshum <mshum002@ucr.edu>
2021-05-05 18:16:08 -0700

    implemented timer to Nicole ver

--- lab3/nicole.js
+++ lab3/nicole.js
@@ -1,6 +1,28 @@
+var timer = new IntervalTimer(fetchTweets, 5000);
 const url = "http://ec2-54-219-224-129.us-west-1.compute.amazonaws.com:2000/feed/random?q=weather";
 const tweetContainer = document.getElementById('contentCenter');
 let dict = {};
+
+const pauseUnpause = clickedButton => {
+  let currText = document.getElementById("timer-button").innerHTML;
+  if (currText == "resume feed") {
+      timer.resume();
+      document.getElementById("timer-button").innerHTML = "pause feed";
+      console.log("success-1");
+  }
+  else if (currText == "pause feed") {
+      timer.pause();
+      document.getElementById("timer-button").innerHTML = "resume feed";
+      console.log("success-2");
+  }
+  else {
+      timer.timeoutCallback();
+      console.log("fail");
+  }
+  console.log(currText);
+}
+
+
 
 const date = (given) => {
   let t = new Date(given);
@@ -37,7 +59,7 @@ const date = (given) => {
   // return month + ' ' + day;
 }
 
-// const fetchTweets = () => {
+function fetchTweets(){
 fetch(url)
   .then(res => res.json())
   .then(data => {
@@ -66,59 +88,40 @@ fetch(url)
     // error catching
     console.log(err);
   })
-// }
+}
 
-// let timer = new IntervalTimer(fetchTweets, 5000);
-// const IntervalTimer = (callback, interval) => {
-//   var timerId, startTime, remaining = 0;
-//   var state = 2; //start on resumed
-//   //0=idle
-//   //1=running
-//   //2=paused
-//   //3=resumed
+function IntervalTimer(callback, interval) {
+  var timerId, startTime, remaining = 0;
+  var state = 2; //start on resumed
+  //0=idle
+  //1=running
+  //2=paused
+  //3=resumed
 
-//   this.pause = function () {
-//       console.log(state, ": pausing");
-//       if (state != 1) return;
+  this.pause = function () {
+      console.log(state, ": pausing");
+      if (state != 1) return;
 
-//       remaining = interval - (new Date() - startTime);
-//       window.clearInterval(timerId);
-//       state = 2;
-//   };
+      remaining = interval - (new Date() - startTime);
+      window.clearInterval(timerId);
+      state = 2;
+  };
 
-//   this.resume = function() {
-//       console.log(state, ": resuming");
-//       if (state != 2) return;
+  this.resume = function() {
+      console.log(state, ": resuming");
+      if (state != 2) return;
 
-//       state = 3;
-//       window.setTimeout(this.timeoutCallback, remaining);
-//   };
+      state = 3;
+      window.setTimeout(this.timeoutCallback, remaining);
+  };
 
-//   this.timeoutCallback = function() {
-//       if (state != 3) return;
+  this.timeoutCallback = function() {
+      if (state != 3) return;
 
-//       callback();
-//       startTime = new Date();
-//       timerId = window.setInterval(callback, interval);
-//       state = 1;
-//   };
-// }
+      callback();
+      startTime = new Date();
+      timerId = window.setInterval(callback, interval);
+      state = 1;
+  };
 
-// const pauseUnpause = clickedButton => {
-//   let currText = document.getElementById("timer-button").innerHTML;
-//   if (currText == "resume feed") {
-//       timer.resume();
-//       document.getElementById("timer-button").innerHTML = "pause feed";
-//       console.log("success-1");
-//   }
-//   else if (currText == "pause feed") {
-//       timer.pause();
-//       document.getElementById("timer-button").innerHTML = "resume feed";
-//       console.log("success-2");
-//   }
-//   else {
-//       timer.timeoutCallback();
-//       console.log("fail");
-//   }
-//   console.log(currText);
-// }
+}
