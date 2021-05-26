3351e23732b7c12721c031f6daeb86c47b0d2e3c
nicohhle <nnguy099@ucr.edu>
2021-05-26 14:20:24 -0700

    restore server.js

--- lab4/controllers/home.js
+++ lab4/controllers/home.js
@@ -2,13 +2,13 @@
 const Room = require('../models/Rooms')
 // Example for handle a get request at '/' endpoint.
 
-fakeDB = () =>{
-  return [
-    {name:"abcd"},
-    {name:"efgh"},
-    {name:"something"}
-  ]
-}
+// fakeDB = () =>{
+//   return [
+//     {name:"abcd"},
+//     {name:"efgh"},
+//     {name:"something"}
+//   ]
+// }
 
 function getHome(request, response){
   // do any work you need to do, then
