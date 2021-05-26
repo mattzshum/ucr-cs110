1b78956435062a4463f555036ca70c9ddfc6e567
nicohhle <nnguy099@ucr.edu>
2021-05-26 10:31:38 -0700

    replace lab4

---
+++ lab4/controllers/home.js
@@ -0,0 +1,24 @@
+// Controller handler to handle functionality in home page
+const Room = require('../models/Rooms')
+// Example for handle a get request at '/' endpoint.
+
+fakeDB = () =>{
+  return [
+    {name:"abcd"},
+    {name:"efgh"},
+    {name:"something"}
+  ]
+}
+
+function getHome(request, response){
+  // do any work you need to do, then
+  Room.find().lean().then(items =>
+    {
+      response.render('home', {title: 'home', rooms: items, isAvailable: true});
+    })
+
+}
+
+module.exports = {
+    getHome
+};
--- lab4/server.js
+++ lab4/server.js
@@ -1,9 +1,23 @@
+// import dependencies
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const hbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const config = require('config');
-const User = require('./models/User');
-const exphbs = require('express-handlebars');
+const path = require('path');
+const Room = require('./models/Rooms');
+
+// import handlers
+const homeHandler = require('./controllers/home.js');
+const roomHandler = require('./controllers/room.js');
+
 const app = express();
+const port = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 const db = config.get('mongoURI');
 
@@ -12,26 +26,34 @@ mongoose
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
-app.engine('hbs', exphbs({
-    defaultLayout: 'main',
-    extname: '.hbs'
-}));
 
+// If you choose not to use handlebars as template engine, you can safely delete the following part and use your own way to render content
+// view engine setup
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.get('/', function (req, res) {
-    User.find()
-    .sort({ date: -1 })
-    .then(items => {    res.render('home', {
-        post: {
-            author: items[0].name,
-            image: 'https://picsum.photos/500/500',
-            comments: []
-        }
-    });});
+// set up stylesheets route
 
-});
+// TODO: Add server side code
 
-app.listen(3000, () => {
-    console.log('The web server has started on port 3000');
-});
+// Create controller handlers to handle requests at each endpoint
+app.post("/create", function(req, res){
+    const newRoom = new Room({
+        name: req.body.roomName
+    })
+    newRoom.save().then(console.log("room added"))
+    .catch(e => console.log(e))
+})
+app.get("/getroom", function(req, res){
+    Room.find().lean().then(items => {
+        res.json(items)
+    })
+
+})
+app.get('/', homeHandler.getHome);
+app.get('/:roomName', roomHandler.getRoom);
+
+// NOTE: This is the sample server.js code we provided, feel free to change the structures
+
+app.listen(port, () => console.log(`Server listening on http://localhost:${port}`));
